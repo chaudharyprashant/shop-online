a6ee73e126f13d22d477ca82b637f7cfec2d8e43
chaudharyprashant <pk15196@gmail.com>
2018-01-28 19:56:23 +0530

    My project setup completed successfully

---
+++ backendshopping/src/main/java/net/pk/backendshopping/App.java
@@ -0,0 +1,13 @@
+package net.pk.backendshopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
